ffc871754bd34240262a1641f9e3a29d862294e6
hyunwoo <hwkim941213@gmail.com>
2019-02-07 10:45:57 +0900

    fix error

--- src/train.py
+++ src/train.py
@@ -59,19 +59,23 @@ X_train, y_train = train_data_loader(pos_dir, neg_dir, do_n4, do_ws, do_resample
 print("\n---------- Start Train ----------")
 
 #########
+## scorer
+scorer = make_scorer(fbeta_score, beta=0.5)
+
+#########
 ## model1
 print("model1")
 model1 = xgb.XGBClassifier(n_jobs=4)
 
 m1_params1 = {
-    'max_depth' : [3,5,7,9,11],
+    'max_depth' : [3,5,7,9],
     'min_child_weight' : [0.5, 1],
     'gamma' : [0, 0.1],
     'subsample' : [0.5, 0.7, 0.9],
     'colsample_bytree' : [0.5, 0.7, 0.9],
 }
 
-m1_grid_1 = GridSearchCV(model1, param_grid=m1_params1, scoring=scorer, cv=5, verbose=0)
+m1_grid_1 = GridSearchCV(model1, param_grid=m1_params1, scoring=scorer, cv=5, verbose=0, n_jobs=4)
 m1_grid_1.fit(X_train, y_train)
 
 best_model1 = m1_grid_1.best_estimator_
@@ -81,10 +85,10 @@ print("Best Params : {}".format(m1_grid_1.best_params_))
 
 m1_params2 = {
     'learning_rate' : [0.01, 0.05, 0.07, 0.1, 0.2],
-    'n_estimators' : [n for n in range(100,1001,50)]
+    'n_estimators' : [n for n in range(500,1501,200)]
 }
 
-m1_grid_2 = GridSearchCV(best_model1, param_grid=m1_params2, scoring=scorer, cv=5, verbose=0)
+m1_grid_2 = GridSearchCV(best_model1, param_grid=m1_params2, scoring=scorer, cv=5, verbose=0, n_jobs=4)
 m1_grid_2.fit(X_train, y_train)
 
 best_model1 = m1_grid_2.best_estimator_
@@ -98,7 +102,7 @@ print("\nmodel2")
 model2 = SVC()
 
 m2_params1 = {
-    'C': [0.001, 0.01, 0.1, 1, 10, 50, 100], 
+    'C': [0.001, 0.01, 0.1, 1, 10, 100], 
     'gamma' : [0.001, 0.01, 0.1, 1, 2, 5, 10, 20],
     'degree' : [2,3,4]
 }
@@ -114,10 +118,10 @@ print("Best Params : {}".format(m2_grid_1.best_params_))
 #########
 ## model3
 print("\nmodel3")
-model3 = LogisticRegression(n_jobs=4)
+model3 = LogisticRegression()
 
 m3_params1 = {
-    'C': [0.001, 0.01, 0.1, 1, 10, 50, 100],
+    'C': [0.001, 0.01, 0.1, 1, 10, 100],
     'max_iter' : [n for n in range(100,1101, 200)]
 }
 
@@ -135,9 +139,8 @@ print("\nmodel4")
 model4 = RandomForestClassifier(n_jobs=4)
 
 m4_params1 = {
-    'max_depth' : [n for n in range(10, 101, 5)],
-    'min_samples_leaf': [1, 2, 3, 4, 5,10, 20, 50],
-    'min_samples_split': [1, 2, 4, 6, 8, 10, 12],
+    'max_depth' : [n for n in range(10, 51, 10)],
+    'min_samples_leaf': [1, 2, 3, 4, 5,10, 20, 50]
 }
 
 m4_grid_1 = GridSearchCV(model4, param_grid=m4_params1, scoring=scorer, cv=5, verbose=0, n_jobs=4)
@@ -166,7 +169,7 @@ print("\nmodel5")
 model5 = LogisticRegression(n_jobs=4, penalty="l1")
 
 m5_params1 = {
-    'C': [0.001, 0.01, 0.1, 1, 10, 50, 100],
+    'C': [0.001, 0.01, 0.1, 1, 10, 100],
     'max_iter' : [n for n in range(100,1101, 200)]
 }
 
@@ -204,7 +207,7 @@ model7 = ElasticNet()
 m7_params1 = {
     'alpha': [0.1, 1, 2, 5, 10, 20, 50, 100],
     'l1_ratio':[0.3, 0.4, 0.5, 0.6], 
-    'max_iter' : [n for n in range(800, 2001, 200)]
+    'max_iter' : [n for n in range(800, 1601, 200)]
 }
 
 m7_grid_1 = GridSearchCV(model7, param_grid=m7_params1, scoring=scorer, cv=5, verbose=0, n_jobs=4)
@@ -221,7 +224,7 @@ print("\nmodel8")
 model8 = Lars()
 
 m8_params1 = {
-    'n_nonzero_coefs': [n for n in range(30, 150, 10)],
+    'n_nonzero_coefs': [n for n in range(30, 150, 20)],
 }
 
 m8_grid_1 = GridSearchCV(model8, param_grid=m8_params1, scoring=scorer, cv=5, verbose=0, n_jobs=4)
@@ -239,7 +242,7 @@ model9 = LassoLars()
 
 m9_params1 = {
     'alpha': [0.1, 1, 2, 5, 10, 20, 50, 100],
-    'max_iter' : [n for n in range(800, 2001, 200)]
+    'max_iter' : [n for n in range(800, 1601, 200)]
 }
 
 m9_grid_1 = GridSearchCV(model9, param_grid=m9_params1, scoring=scorer, cv=5, verbose=0, n_jobs=4)
