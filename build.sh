9a3a0bb8609a20fd051cecd3b670680d3f0e954d
SPark9625 <SPark9625@gmail.com>
2019-02-15 18:54:44 +0000

    build.sh changed

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -f ./submission.tar.gz ]; then
-    echo "Removing submission.tar.gz..."
-    rm ./submission.tar.gz
-fi
-
-docker image rm submission:0.0.1
 docker build --tag submission:0.0.1 .
+
+echo "Compressing..."
+
 docker save submission:0.0.1 | gzip > submission.tar.gz
