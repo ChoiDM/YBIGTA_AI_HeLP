9774b5ac26341a5d3a706395f851edf82ba86a32
Semin Park <SPark9625@gmail.com>
2019-02-20 04:27:32 +0900

    #2 under construction

--- src/train.py
+++ src/train.py
@@ -7,6 +7,8 @@ from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import fbeta_score, make_scorer
+
+import pandas as pd
 import pickle
 import datetime
 import pprint
@@ -60,59 +62,83 @@ xgb_params = {
     'max_depth': [3, 4, 5]
 }
 
-# rf_params = {
-#     'n_estimators': [10,50,100],
-#     'criterion': ['gini', 'entropy'],
-#     'max_depth': [None, 5, 10, 20],
-#     'min_samples_split': [2, 3, 5, 10],
-#     'max_features': ['sqrt', 'log2']
-# }
+log_params = {
+    'penalty': ['l1', 'l2'],
+    'tol': [1e-3, 1e-4, 1e-5],
+    'C': [0.5, 1, 2],
+}
+
+sgd_params = {
+    'penalty': ['l1', 'l2', 'elasticnet'],
+    'alpha': [1e-3, 1e-4, 1e-5],
+    'max_iter': [1e+3, 2e+3, 4e+3]
+}
+
+svc_params = {
+    'penalty': ['l1', 'l2'],
+    'tol': [1e-3, 1e-4, 1e-5],
+    'C': [0.5, 1, 2],
+}
+
+# ----------------------- Number of Combinations -------------------------- #
+xgb_comb = np.prod([ len(val) for val in xgb_params.values() ])
+log_comb = np.prod([ len(val) for val in log_params.values() ])
+sgd_comb = np.prod([ len(val) for val in sgd_params.values() ])
+svc_comb = np.prod([ len(val) for val in svc_params.values() ])
+combos = [xgb_comb, log_comb, sgd_comb, svc_comb]
+
+# scorer
+scorer = make_scorer(fbeta_score, beta=0.5)
 
 
 # --------------------------- Model definitions ---------------------------- #
 
 xgb_base = xgb.XGBClassifier(learning_rate=0.01, n_estimators=600, objective='binary:logistic', silent=True, nthread=1)
+log_base = LogisticRegression()
+sgd_base = SGDClassifier()
+svc_base = LinearSVC(dual=False)
+bases = [xgb.XGBClassifier, LogisticRegression, SGDClassifier, LinearSVC]
+
+xgb_model = RandomizedSearchCV(xgb_base, xgb_params, cv=5, scoring=scorer, verbose=3, n_jobs=-1)
+log_model = RandomizedSearchCV(log_base, log_params, cv=5, scoring=scorer, verbose=3, n_jobs=-1)
+sgd_model = RandomizedSearchCV(sgd_base, sgd_params, cv=5, scoring=scorer, verbose=3, n_jobs=-1)
+svc_model = RandomizedSearchCV(svc_base, svc_params, cv=5, scoring=scorer, verbose=3, n_jobs=-1)
+models = [xgb_model, log_model, sgd_model, svc_model]
+
+# -------------------------- 1st Model fitting ----------------------------- #
+
+for model in models:
+    model.fit(X_train, y_train)
 
 
-xgb_model = RandomizedSearchCV(xgb_base, xgb_params, cv=5, scoring=make_scorer(fbeta_score, beta=0.5),
-                               verbose=3, n_jobs=-1)
-logistic = LogisticRegression()
-sgd = SGDClassifier()
-svc = LinearSVC()
+# ---------------------------- Ensemble ------------------------------------ #
 
-final_model = VotingClassifier(estimators=[('xgb', xgb_model), ('logistic', logistic), ('sgd', sgd), ('svc', svc)], voting="hard")
+estimators = []
+for comb, model, base in zip(combos, models, bases):
+    n = comb // 10  # use top 10% of the models
+
+    df = pd.DataFrame(model.cv_results_)
+    score = df['mean_train_score']
+    score.index = df['params']
+    score = score.sort_values()
+
+    params = list(score.index)[-n:]
+
+    for i, p in enumerate(params):
+        estimators.append((base.__name__ + '_' + str(i), base(**p)))
 
 
-# ---------------------------- Model fitting ------------------------------- #
-# xgb_model.fit(X_train, y_train)
-# lda_model.fit(X_train, y_train)
-# logistic.fit(X_train, y_train)
-# sgd.fit(X_train, y_train)
-# svc.fit(X_train, y_train)
+final_model = VotingClassifier(estimators=estimators, voting="hard")
+
+
+# ---------------------------- Model re-fitting ---------------------------- #
+
 
 final_model.fit(X_train, y_train)
 print(final_model)
 
 
 
-# print("Base model: XGBoost")
-# print("Best params:", xgb_model.best_params_)
-# print("Best estimator:\n", xgb_model.best_estimator_)
-# print()
-
-# print("Rest of the models:")
-# print(lda_model)
-# print(logistic)
-# print(sgd)
-# print(svc)
-
-
-
 # ----------------------------- Save model --------------------------------- #
-# pickle.dump(xgb_model, open('/data/model/xgb_model.pickle.dat', 'wb'))
-# pickle.dump(lda_model, open('/data/model/lda_model.pickle.dat', 'wb'))
-# pickle.dump(logistic, open('/data/model/logistic.pickle.dat', 'wb'))
-# pickle.dump(sgd, open('/data/model/sgd.pickle.dat', 'wb'))
-# pickle.dump(svc, open('/data/model/svc.pickle.dat', 'wb'))
 
 pickle.dump(final_model, open('/data/model/final_model.pickle.dat', 'wb'))
