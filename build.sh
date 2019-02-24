b05c414470e46df660a92e7f17603850ff94e1b7
Taeoh Kim <kimtaeoh95@gmail.com>
2019-02-25 02:15:43 +0900

    Add auto image build

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -f ./submission.tar.gz ]; then
+    echo "Removing submission.tar.gz..."
+    docker image rm submission:0.0.1
+    rm ./submission.tar.gz
+fi
+
+docker build --tag submission:0.0.1 .
+echo "Build is finished... Start zip file"
+
+docker save submission:0.0.1 | gzip > submission.tar.gz
