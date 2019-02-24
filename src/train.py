af682f7369d59c436a573a283a4c42c801be1330
Taeoh Kim <kimtaeoh95@gmail.com>
2019-02-25 03:10:19 +0900

    Change indent error

--- src/train.py
+++ src/train.py
@@ -1,8 +1,10 @@
-from utils.data_loader import train_data_loader
+from utils.cube_data_loader import train_data_loader
 from sklearn.metrics import fbeta_score, make_scorer
 import xgboost as xgb
 import pickle
 import datetime
+import keras
+import numpy as np
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -13,8 +15,8 @@ print("Start:", time)
 
 
 # Print Information
-name = 'master-branch'
-model = 'XGBoost'
+name = 'taeoh_base'
+model = 'CNN'
 summary = 'Normalization with new method / Threshold to 0.70'
 
 print('---------------------------')
@@ -35,6 +37,8 @@ norm = 'new' # 'norm' should be 'ws' or 'new'
 do_resample = True
 
 do_shuffle = False
+brain_mask=True
+# True when mask brain and False when mask only inf
 
 
 #########################################################################################################################
@@ -43,12 +47,14 @@ do_shuffle = False
 
 
 # Data Load
-X_train, y_train = train_data_loader(pos_dir, neg_dir, norm, do_resample, do_shuffle, features, target_voxel)
+X_ADC, X_FLAIR, y_train = train_data_loader(pos_dir, neg_dir, norm, do_resample, do_shuffle, features, target_voxel, brain_mask)
 
+X_train = np.hstack([X_ADC, X_FLAIR])
+print(X_train.shape)
 
 # Fit Model with Training Data
-xgbClassifier = xgb.XGBClassifier(subsample=1.0, gamma=2, colsample_bytree=0.8, max_depth=6, min_child_weight=10)
-xgbClassifier.fit(X_train, y_train)
+# xgbClassifier = xgb.XGBClassifier(subsample=1.0, gamma=2, colsample_bytree=0.8, max_depth=6, min_child_weight=10)
+# xgbClassifier.fit(X_train, y_train)
 
 
 # Use F0.5 score if necessary
