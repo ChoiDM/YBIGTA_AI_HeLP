34f45ab64ced568aadb034669ae6602e31a1e71f
Choi Dong Min <35302641+ChoiDM@users.noreply.github.com>
2019-03-03 02:17:45 +0900

    add 'index_check' to avoid index error

--- src/utils/cube_tools.py
+++ src/utils/cube_tools.py
@@ -1,16 +1,6 @@
-from glob import glob
-import nibabel as nib
 import numpy as np
-import pandas as pd
-import os
-from random import shuffle
-import datetime
 
-from utils.Normalization import normalization
-from utils.Resample import resample, mask2binary
-from utils.WhiteStripeNormalization import ws_normalize
 
-from concurrent.futures import ProcessPoolExecutor
 
 def get_center(mask_array):
     x_values, y_values, z_values = np.where(mask_array > 0.5)
@@ -27,7 +17,6 @@ def get_center(mask_array):
     else:
         return None
 
-
 def cube_indexing(center_point, cube_size):
     half_size = cube_size // 2
 
@@ -41,14 +30,45 @@ def cube_indexing(center_point, cube_size):
     
     return indexing
 
+def index_check(img_array, cube_center, cube_shape):
+    x_center, y_center, z_center = cube_center
+    x_max, y_max, z_max = img_array.shape
+    x_size, y_size, z_size = np.array(cube_shape)
+    
+    x_half = x_size // 2
+    y_half = y_size // 2
+    z_half = z_size // 2
+
+    # X axis
+    if x_center - x_half < 0:
+        x_center += (x_half - x_center)
+    elif (x_half - x_center) > x_max:
+        x_center -= (x_half - x_center)
+
+    # Y axis
+    if y_center - y_half < 0:
+        y_center += (y_half - y_center)
+    elif (y_half - y_center) > y_max:
+        y_center -= (y_half - y_center)
+
+    # Z axis
+    if z_center - z_half < 0:
+        z_center += (z_half - z_center)
+    elif (z_half - z_center) > z_max:
+        z_center -= (z_half - z_center)
+    
+    return (x_center, y_center, z_center)
+
+
     
 def img2cube(img_array, cube_center, cube_shape):
     cube_array = np.copy(img_array)
 
     x_size, y_size, z_size = np.array(cube_shape)
+    x_center, y_center, z_center = index_check(img_array, cube_center, cube_shape)
 
     if not (x_size > img_array.shape[0] or y_size > img_array.shape[1] or z_size > img_array.shape[2]):
-        x_center, y_center, z_center = cube_center
+        
         
         x_index = cube_indexing(x_center, x_size)
         y_index = cube_indexing(y_center, y_size)
