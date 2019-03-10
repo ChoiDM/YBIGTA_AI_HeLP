a0f15111bfc8bb77d36253f0e34c6b33e81e9b14
Choi Dong Min <35302641+ChoiDM@users.noreply.github.com>
2019-03-10 20:47:40 +0900

    adapt hyper-parameters

--- src/train.py
+++ src/train.py
@@ -23,7 +23,6 @@ neg_dir = path+"/train/negative/"
 
 
 # Setting
-random_state = 1213
 deep = False
 BETA = 0.75            # for Hyperparameter Tuning (Use F 0.75 Score)
 BETA2 = 0.5            # for Stacking
@@ -54,74 +53,65 @@ X_train, y_train = train_data_loader(pos_dir, neg_dir, norm, do_resample, do_shu
 #------------------------------------------------------------------------------------------------------------------------#
 
 
-# Model (1)
-print("model1")
-m1_params1 = {'subsample': [0.6], 'colsample_bytree': [0.6], 'min_child_weight': [0.5], 'probability': [True], 
-              'gamma': [3.0], 'n_estimators': [300], 'learning_rate': [0.01], 'max_depth': [7], 'random_state' : [1213]}
-model1 = ml_xgb(X_train, y_train, cv=cv, beta=BETA, params=None, random_state=random_state)
+# Model (1) - XGBoost
+m1_params = {'subsample': [0.6], 'colsample_bytree': [1.0], 'min_child_weight': [1], 
+              'gamma': [2.5], 'n_estimators': [500], 'learning_rate': [0.1], 'max_depth': [8]}
+model1 = ml_xgb(X_train, y_train, cv=cv, beta=BETA, params=m1_params, random_state=1213)
 
 
-# Model (2)
-print("\nmodel2")
-m2_params1 = {'probability': [True], 'degree': [2], 'C': [0.001], 'gamma': [0.001]}
-model2 = ml_svm(X_train, y_train, cv=cv, beta=BETA, params=m2_params1)
+# Model (2) - SVM
+m2_params = {'probability': [True], 'degree': [2], 'C': [0.001], 'gamma': [0.001]}
+model2 = ml_svm(X_train, y_train, cv=cv, beta=BETA, params=m2_params)
 
 
-# Model (3)
-print("\nmodel3")
+# Model (3) - Logistic Regression
 model3 = ml_logistic(X_train, y_train, cv=cv, beta=BETA)
 
 
-# Model (4)
-print("\nmodel4")
-m4_params1 = {'n_estimators': [500], 'min_samples_leaf': [50], 'max_depth': [15], 'random_state' : [1213]}
-model4 = ml_rf(X_train, y_train, cv=cv, beta=BETA, params=None, random_state=random_state)
+# Model (4) - Random Forest
+m4_params = {'n_estimators': [300], 'min_samples_leaf': [20], 'max_depth': [6]}
+model4 = ml_rf(X_train, y_train, cv=cv, beta=BETA, params=m4_params, random_state=1213)
 
 
-# Model (5)
-print("\nmodel5")
-m5_params1 = {'penalty': ['l1'], 'C': [1], 'max_iter': [900]}
-model5 = ml_lasso(X_train, y_train, cv=cv, beta=BETA, params=m5_params1)
+# Model (5) - Lasso
+m5_params = {'penalty': ['l1'], 'C': [1], 'max_iter': [900]}
+model5 = ml_lasso(X_train, y_train, cv=cv, beta=BETA, params=m5_params)
 
 
-# Model (6)
-print("\nmodel6")
-m6_params1 =  {'alpha': [10], 'max_iter': [None]}
-model6 = ml_ridge(X_train, y_train, cv=cv, beta=BETA, params=m6_params1)
+# Model (6) - Ridge
+m6_params =  {'alpha': [10], 'max_iter': [None]}
+model6 = ml_ridge(X_train, y_train, cv=cv, beta=BETA, params=m6_params)
 
 
-# Model (7)
-print("\nmodel7")
-m7_params1 =  {'penalty': ['elasticnet'], 'loss': ['log'], 'alpha': [100], 'l1_ratio': [0.5], 'max_iter': [1400]}
-model7 = ml_elasticNet(X_train, y_train, cv=cv, beta=BETA, params=m7_params1)
+# Model (7) - ElasticNet
+m7_params =  {'penalty': ['elasticnet'], 'loss': ['log'], 'alpha': [100], 'l1_ratio': [0.5], 'max_iter': [1400]}
+model7 = ml_elasticNet(X_train, y_train, cv=cv, beta=BETA, params=m7_params)
 
 
-# Model (8)
-print("\nmodel8")
-m8_params1 =  {'n_nonzero_coefs': [70]}
-model8 = ml_lars(X_train, y_train, cv=cv, beta=BETA, params=m8_params1)
+# Model (8) - Lars
+m8_params =  {'n_nonzero_coefs': [70]}
+model8 = ml_lars(X_train, y_train, cv=cv, beta=BETA, params=m8_params)
 
 
-# Model (9)
-print("\nmodel9")
-m9_params1 =  {'alpha': [0.1], 'max_iter': [800]}
-model9 = ml_larsLasso(X_train, y_train, cv=cv, beta=BETA, params=m9_params1)
+# Model (9) - LarsLasso
+m9_params =  {'alpha': [0.1], 'max_iter': [800]}
+model9 = ml_larsLasso(X_train, y_train, cv=cv, beta=BETA, params=m9_params)
 
 
-# Model (10)
-print("\nmodel10")
-m10_params1 =  {'n_estimators': [50], 'max_depth': [3], 'random_state' : [1213]}
-model10 = ml_extraTrees(X_train, y_train, cv=cv, beta=BETA, params=None, random_state=random_state)
+# Model (10) - Extra Trees
+m10_params =  {'n_estimators': [10], 'max_depth': [5]}
+model10 = ml_extraTrees(X_train, y_train, cv=cv, beta=BETA, params=m10_params, random_state=1213)
 
 
-# Model (11)
-print("\nmodel11")
-model11 = ml_adaboost(X_train, y_train, cv=cv, beta=BETA, params=None, random_state=random_state)
+# Model (11) - AdaBoost
+m11_params = {'algorithm' : ['SAMME.R'], 'learning_rate' : [0.01], 'n_estimators' : [300]}
+model11 = ml_adaboost(X_train, y_train, cv=cv, beta=BETA, params=m11_params, random_state=1213)
 
 
-# Model (12)
-print("\nmodel12")
-model12 = ml_lightgbm(X_train, y_train, cv=cv, beta=BETA, params=None, random_state=random_state)
+# Model (12) - LightGBM
+m12_params = {'subsample' : [0.6], 'colsample_bytree' : [1.0], 'reg_alpha' : [5.0], 'reg_lambda' : [2.0],
+              'learning_rate' : [0.1], 'n_estimators' : [100], 'min_child_weight' : [5]}
+model12 = ml_lightgbm(X_train, y_train, cv=cv, beta=BETA, params=m12_params, random_state=1213)
 #------------------------------------------------------------------------------------------------------------------------
 
 
@@ -165,7 +155,6 @@ for meta in [meta_xgb, meta_logistic, meta_NN, meta_weight] :
 
     
 # Print result
-print("\n")
 print(making_result(S_train, y_pred_lst, y_pred_binary_lst, y_pred_lst2, y_pred_binary_lst2, include_model, include_model2, include_model3, y_train))\
 
 #------------------------------------------------------------------------------------------------------------------------
